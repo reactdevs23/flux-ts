bbddfc0938ab5feb89f27a99095cc2a44065b3ee
dalimkumar604 <dalimkumar212582@gmail.com>
2024-11-18 02:56:10 +0600

    complete

--- src/components/ImageQuality/ImageQuality.tsx
+++ src/components/ImageQuality/ImageQuality.tsx
@@ -1,25 +1,31 @@
 "use client";
-import React, { useRef, useState, useEffect } from "react";
+
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import { fishImg, lavenderPath, sportsCar } from "@/images";
+import { lavenderPath, sportsCar } from "@/images";
 import { MdOutlineChevronLeft, MdOutlineChevronRight } from "react-icons/md";
 import Slide from "./Slide";
-import useSpacing from "../../Hook/Hook";
+import useSpacing from "@/Hook/Hook";
 
-const ImageQuality = () => {
+interface SlideData {
+  img: any; // Image path or URL
+  title: string;
+}
+
+const PrecisePromt = () => {
   const { dynamicPaddingLeft } = useSpacing(1232);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   const swiperRef = useRef<any>(null);
 
   const handleSlideChange = (swiper: any) => {
     setActiveIndex(swiper.activeIndex); // Track the active index
   };
 
-  const data = [
-    { img: fishImg, title: "Lavender Path to Serenity" },
+  const data: SlideData[] = [
+    { img: lavenderPath, title: "Lavender Path to Serenity" },
     { img: sportsCar, title: "Sleek Sports Car in Garage" },
     { img: lavenderPath, title: "Lavender Path to Serenity" },
     { img: sportsCar, title: "Sleek Sports Car in Garage" },
@@ -27,31 +33,27 @@ const ImageQuality = () => {
 
   return (
     <section className="bg-black700 py-4 sm:py-6 lg:py-10">
-      <div
-        className="spacing-left section-spacing   screen1340:pl-auto   screen1340:w-full  	mx-auto  md:ml-auto py-32 flex flex-col gap-10 md:gap-10"
+      <section
+        className="spacing-left section-spacing screen1340:pl-auto screen1340:w-full mx-auto md:ml-auto py-32 flex flex-col gap-10 md:gap-10"
         style={
           {
             "--spacing-left": `${dynamicPaddingLeft}px`,
           } as React.CSSProperties
         }
       >
-        <div
-          className={
-            "max-w-[773px] w-full md:w-11/12	flex flex-col gap-2 sm:gap-4 md:gap-4"
-          }
-        >
+        <div className="max-w-[773px] w-full md:w-11/12 flex flex-col gap-2 sm:gap-4 md:gap-4">
           <h2 className=" text-white text-[40px] md:text-[50px] lg:text-6xl  font-bold leading-tight">
             Outstanding Image Quality, Lifelike Details
           </h2>
           <p className="text-base md:text-xl text-white ">
             FLUX.1 generates images that surpass similar products in quality.
             From delicate textures to subtle lighting, every pixel showcases
-            extraordinary quality.
+            extraord{" "}
           </p>
         </div>
         <Swiper
           modules={[Navigation]}
-          onSlideChange={handleSlideChange} // Handle slide change
+          onSlideChange={handleSlideChange}
           loop={true}
           slidesPerView={2}
           spaceBetween={30}
@@ -87,21 +89,21 @@ const ImageQuality = () => {
         {/* Custom Navigation Buttons */}
         <div className="hidden md:flex justify-end gap-4 mx-auto max-w-[790px] w-full">
           <button
-            className="arrowButton"
+            className="arrowButton opacity-80"
             onClick={() => swiperRef.current?.swiper.slidePrev()}
           >
             <MdOutlineChevronLeft className="text-black text-xl" />
           </button>
           <button
-            className="arrowButton"
+            className="arrowButton opacity-80"
             onClick={() => swiperRef.current?.swiper.slideNext()}
           >
             <MdOutlineChevronRight className="text-black text-xl" />
           </button>
         </div>
-      </div>
+      </section>
     </section>
   );
 };
 
-export default ImageQuality;
+export default PrecisePromt;
